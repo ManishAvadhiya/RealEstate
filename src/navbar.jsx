75d105a3fda5575ffffb4ede286c64cc55c954e2
ManishAvadhiya <avadhiyamanish@gmail.com>
2024-09-10 23:27:42 +0530

    "changed"

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="left">
             <a href="/" className='logo'>
             <img src="/logo.png" alt="" />
-            <span>ManishEstate</span>
+            <span>Manish</span>
             </a>
             <a href="/">Home</a>
             <a href="/">About</a>
